b8c9056ca1c10f5f559f66b0209936e823d696af
ovenslove <1905997838@qq.com>
2017-04-02 03:21:59 +0800

    修复配置文件

--- config/config.js
+++ config/config.js
@@ -1,6 +1,5 @@
 module.exports = {
-    "apiServerUrl": "api.jqstudy.cn",
-    "appId": "wxa1d0f9d674b365c6",
+    "apiServerUrl": "https://api.jqstudy.cn",
     "staticServerUrl": 'https://xcx-xiangpiaoqc.b0.upaiyun.com/v0.1/',
     "baiduMapKey": 'u2g4P3PAHZQE5NL1wGcjyFKK1lKUUdDO'
 };
